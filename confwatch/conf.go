7c5f04a01f926594fc3294e2d24335e8ef2bf7da
thiagohmm <thiagohmm@gmail.com>
2024-06-15 14:17:59 -0300

    carregando com arquivo de configuracao com viper

--- confwatch/conf.go
+++ confwatch/conf.go
@@ -1,10 +1,7 @@
 package confwatch
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"os"
+	"github.com/spf13/viper"
 )
 
 const configFile = "config.json"
@@ -20,82 +17,17 @@ type AppConfig struct {
 	AnsibleLocation       string `json:"ansible_location"`
 }
 
-func RetornaConf(cn string) (string, error) {
+func LoadConfig(path string) (*AppConfig, error) {
+	var c AppConfig
+	viper.SetConfigFile(path)
+	viper.SetConfigType("json")
+	viper.AutomaticEnv()
+	viper.SetEnvPrefix("confwatch")
+	viper.Unmarshal(&c)
 
-	var config AppConfig
-	configFile, err := os.ReadFile(configFile)
-	if err != nil {
-		log.Fatalf("Erro ao ler o arquivo de configuração: %v", err)
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, err
 	}
 
-	err = json.Unmarshal(configFile, &config)
-	if err != nil {
-		log.Fatalf("Erro ao fazer unmarshal do arquivo de configuração: %v", err)
-
-	}
-
-	switch cn {
-	case "AnsiblePlaybookComand":
-		if config.AnsiblePlaybookComand == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-		return config.AnsiblePlaybookComand, nil
-
-	case "WatchFolder":
-		if config.WatchFolder == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-
-		return config.WatchFolder, nil
-
-	case "LogErrorFile":
-		if config.LogErrorFile == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-
-		return config.LogErrorFile, nil
-
-	case "LogAnsibleFile":
-		if config.LogAnsibleFile == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-		return config.LogAnsibleFile, nil
-
-	case "MapAnsibleFile":
-		if config.MapAnsibleFile == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-		return config.MapAnsibleFile, nil
-
-	case "LogExtension":
-		if config.LogExtension == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-		return config.LogExtension, nil
-
-	case "ConfigurationMap":
-		if config.ConfigurationMap == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-		return config.ConfigurationMap, nil
-
-	case "AnsibleLocation":
-		if config.AnsibleLocation == "" {
-			return "", fmt.Errorf("Chave Nula")
-		}
-		return config.AnsibleLocation, nil
-
-	default:
-		return "Error", os.ErrExist
-	}
-}
-
-func CheckIfFileIsBlank(arquivo string) (string, error) {
-	file, err := os.ReadFile(arquivo)
-
-	if err != nil {
-		log.Fatalf("unable to read file: %v", err)
-	}
-
-	return string(file), nil
+	return &c, nil
 }
