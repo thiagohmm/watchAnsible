d338b32fce868dd37d8738ef4fecaa9d0f5e8ac8
thiagohmm <thiagohmm@gmail.com>
2024-06-15 14:17:20 -0300

    alterando carregamento de arquivo

--- loggwatch/logger.go
+++ loggwatch/logger.go
@@ -8,10 +8,10 @@ import (
 )
 
 func SetupLogger() (*zap.Logger, error) {
-	var errorFile, _ = confwatch.RetornaConf("LogErrorFile")
+	var errorFile, _ = confwatch.LoadConfig("../config.json")
 
 	config := zap.NewProductionConfig()
-	config.OutputPaths = []string{errorFile} // Nome do arquivo de log
+	config.OutputPaths = []string{errorFile.LogErrorFile} // Nome do arquivo de log
 	config.EncoderConfig = zapcore.EncoderConfig{
 		TimeKey:        "timestamp",
 		LevelKey:       "level",
